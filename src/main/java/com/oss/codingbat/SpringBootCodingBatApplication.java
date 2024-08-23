e24e94b42b08d6edf7882266d8595ec8c9db5ce8
Hasan Buriyev <hasanburiyev0@gmail.com>
2024-08-23 17:14:06 +0500

    first commit

---
+++ src/main/java/com/oss/codingbat/SpringBootCodingBatApplication.java
@@ -0,0 +1,13 @@
+package com.oss.codingbat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodingBatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodingBatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oss/codingbat/SpringBootCodingBatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.codingbat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodingBatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
